a008ba2d5396e08df241e5119d92e96df998d95f
Prakash Mohit <prakashmohit21x@gmail.com>
2025-07-30 22:03:47 +0530

    Updated firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,9 @@
-// firebase.js
 const admin = require("firebase-admin");
 
 const firebaseConfig = JSON.parse(process.env.FIREBASE_KEY);
+
+// Fix the private key by replacing escaped newlines
+firebaseConfig.private_key = firebaseConfig.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(firebaseConfig),
